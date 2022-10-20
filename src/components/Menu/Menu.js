d46ace066887d16d0f700914ac36680fd8a49393
Alex J Garcia <alexshuu@gmail.com>
2022-10-20 11:16:47 -0400

    Indicate the current page

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,20 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Menu.css';
 import 'remixicon/fonts/remixicon.css';
 
 export default function Menu() {
+  const {pathname} = useLocation();
+
   const menuConfig = [
     ['Home', 'home'],
     ['Blog', 'bookmark'],
     ['Portfolio', 'briefcase']
   ];
 
-  const menuItems = menuConfig.map((menuItem, index) => {
-    const [item, icon] = menuItem;
-
+  const menuItems = menuConfig.map(([item, icon], index) => {
+    const isCurrent = item === pathname.slice(1) || (pathname === '/' && item == 'Home');
     return (
       <li key={index} className='Menu-item'>
-        <Link to={`/${item === 'home' ? '' : item}`}>
+        <Link
+          to={`/${item === 'Home' ? '' : item}`}
+          className={isCurrent ? 'current' : null}
+          aria-current={isCurrent ? 'page' : null}
+        >
           <i className={`ri-${icon}-line`}></i>
           <span>{item}</span>
         </Link>
